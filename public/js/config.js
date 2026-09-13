5a1f2d7315f96bc7f4a20160176e4d470b923005
kyzenotdev <rizkiehermawan313@gmail.com>
2026-09-13 14:55:36 +0700

    Update config.js

--- public/js/config.js
+++ public/js/config.js
@@ -1,7 +1,7 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
 // Kredensial Supabase
-const SUPABASE_URL = 'https://kfjvekitgipyiroixxti.supabase.co'; // Ganti dengan Project URL asli kamu dari Supabase
+const SUPABASE_URL = 'https://bxvgaymtuxnjsbgulqxd.supabase.co'; // Ganti dengan Project URL asli kamu dari Supabase
 const SUPABASE_ANON_KEY = 'sb_publishable_GB_9r-6O8AdrjLlSguvUBg_vCmiRsYa';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
